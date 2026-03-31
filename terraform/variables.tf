7b54e17f394466f46ebc0c61bd58fcaaeeaff25c
devops <devops>
2026-03-31 09:35:52 -0300

    fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "gitopsEKS"
+  default     = "git-ops-EKS"
 }
 ##
 ##
